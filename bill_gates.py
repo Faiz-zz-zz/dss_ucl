2f85f02914b114479a49e7ceba72905ee97648f4
Faiz <abdulfaizp@gmail.com>
2016-02-06 17:00:41 +0000

    first predsictions are up!

--- bill_gates.py
+++ bill_gates.py
@@ -1,6 +1,20 @@
 import csv
 import re
 
+def regressionLine(array):
+	yearArray = [i for i in range(2005, 2014)]
+	xDash = sum(yearArray)/len(yearArray)
+	n = len(array)
+	yDash = sum(array)/n
+	b = ((n*sum([a*b for a, b in zip(yearArray, array)]))-(sum(array)*sum(yearArray)))/((n*sum(map(lambda x: x**2, yearArray)))-((sum(yearArray))**2))
+	a = ((sum(array))-b*sum(yearArray))/n
+	for i in [2020, 2025, 2030]:
+		array.append(a+b*i)
+
+
+
+
+
 with open("carbon.csv", 'r') as f:
     data = [row for row in csv.reader(f.read().splitlines())]
 number = len(data)
@@ -10,12 +24,15 @@ names = []
 values = []
 
 for i in range(1, number):
-	names.append(data[i][1])
 	try:
-		values.append(map(lambda d:float(d), map(lambda s:re.sub(',','',s),data[i][2:])))
+		array = map(lambda s:re.sub(',','',s),data[i][2:])
+		array = map(lambda d:float(d), array)
+		regressionLine(array)
+		values.append(array)
+		names.append(data[i][1])
 	except:
 		pass
-	# array = map(lambda s:re.sub(',','',s),data[i][2:])
+	# # array = map(lambda s:re.sub(',','',s),data[i][2:])
 print values
 print names	
 
